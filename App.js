19fd0a2335e9a7ef8879ebde3893788008a08730
Dallin Whiting <dallinkwhiting@gmail.com>
2024-01-15 14:11:21 -0700

    Set up navigation tabs

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BottomNav from './components/BottomNav';
+import Login from './components/Login';
 
-export default function App() {
+const App = () => {
+  const [user, setUser] = useState('user')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    {!user
+      ? <Login />
+      : <NavigationContainer>
+          <BottomNav />
+        </NavigationContainer>
+    }
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ components/BottomNav.js
@@ -0,0 +1,16 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from './Dashboard';
+import Settings from './Settings';
+
+const Tab = createBottomTabNavigator();
+
+function BottomNav() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={Dashboard} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+}
+
+export default BottomNav
