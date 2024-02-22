05ccc5806fdd6ece23298cabd6d699c9a0837028
Dallin Whiting <dallinkwhiting@gmail.com>
2024-02-22 08:10:21 -0700

    Init friends page

---
+++ components/FriendsPage.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const FriendsPage = () => {
+	return (
+		<View
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				height: '100%',
+			}}
+		>
+			<Text style={{ fontSize: 40 }}>Coming Soon</Text>
+		</View>
+	);
+};
+
+export default FriendsPage;
